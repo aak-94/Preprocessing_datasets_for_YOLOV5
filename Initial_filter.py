7740ac1616b4b2ab6ec3cf03363c75354943a643
akshay <akshaykhot1994@gmail.com>
2021-10-24 14:30:13 +0200

    + changes for new feature
    + function inputs updated to handle the new tags in json file
    +  marking of images after every 100  files

--- Initial_filter.py
+++ Initial_filter.py
@@ -26,7 +26,9 @@ def main(data_dir,destination):
         print(key+' datasets total_annotaitons: ', len(xml_list[key]))
         print(key+' dataset total_images: ', len(image_list))
 
-    unique_class_names, all_class_names= get_class_names(xml_list)
+    all_class_names= get_class_names(xml_list)
+    unique_class_names,all_class_names=replace_del_names(all_class_names,data_dir['replace_del'])
+    print(unique_class_names)
     create_save_bar_chart(unique_class_names, all_class_names,'Class distribution of filtered dataset',result_dir)
     total_images = 0
     img_size=data_dir['img_size']
@@ -34,11 +36,11 @@ def main(data_dir,destination):
         for i,file in enumerate(images[key]):  #processing one image 
             xml_name = file[:-4]+'.xml'
             if os.path.isfile(os.path.join(annotation_path[key],xml_name)):  #checking if corresponding xml exists
-                dict,reshaping = xml_to_dict(annotation_path[key], xml_name, image_path[key], file, img_size, output_xmls,out_img)    
+                dict,reshaping = xml_to_dict(annotation_path[key], xml_name, image_path[key], file, data_dir, output_xmls,out_img)    
                 if len(dict)!=0:                                        #checking if xml has an trainable object 
                     dict_to_yolo_label(file,dict,img_size,unique_class_names,label_path)
                     total_images+=1
-                    if i % 10 ==0:
+                    if i % 100 ==0:
                         'print every 10th images for observation' 
                         write_marked_images(image_path[key], file, dict, obs_path,reshaping,img_size)
                 else:
@@ -47,8 +49,8 @@ def main(data_dir,destination):
                 print('Annotation file for',file,'not found')
     print('total_valid_images:', total_images)
     train_test_split(total_images,train_test_paths,out_img,label_path,output_xmls)
-    create_data_representation(train_test_paths[0],seperator,'train dataset',result_dir)
-    create_data_representation(train_test_paths[4],seperator,'test dataset',result_dir)
+    create_data_representation(train_test_paths[0],seperator,'train dataset',result_dir,data_dir['replace_del'])
+    create_data_representation(train_test_paths[4],seperator,'test dataset',result_dir,data_dir['replace_del'])
 
 if __name__ =="__main__":
     with open('./datasets.json') as json_params:
